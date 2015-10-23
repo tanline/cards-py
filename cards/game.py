54708db94b2571d656098095bd6f4b1e0a9c30fc
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2015-10-23 11:46:42 -0400

    Update how id is gotten from player

--- cards/game.py
+++ cards/game.py
@@ -1,3 +1,5 @@
+from player import Player
+
 class Game(object):
 
     def __init__(self, deck, game_type=None):
@@ -5,10 +7,10 @@ class Game(object):
         self.deck = deck
 
     def add_player(self, player):
-        self._players[player.get_id] = player
+        self._players[player.id()] = player
 
     def remove_player(self, player):
-        del self._players[player.id]
+        del self._players[player.id()]
 
     def number_of_players(self):
         return len(self._players)
