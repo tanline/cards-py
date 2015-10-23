017bfa1fc52762a2e0508c0850b6b0941d81766a
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2015-10-23 18:02:57 -0400

    Created War.

--- cards/__main__.py
+++ cards/__main__.py
@@ -1,31 +1,6 @@
-from deck import Deck
-from game import Game
+from game import War
 
 if __name__ == '__main__':
-    d = Deck()
-    # a = d.deal()
-    # b = d.deal()
-    # c = d.deal()
+    game = War()
+    game.start_game()
 
-    # print "Card A is", a
-    # print "Card B is", b
-    # print "Card C is", c
-    # print "Is the value of A less than B?", a<b
-    # print "Is the value of A less than C?", a<c
-    # print "Is the value of A less than B?", a<b
-    # print "Is the value of B less than C?", b<c
-
-    game = Game(d)
-    print "Number of players in the game", game.number_of_players()
-    print "Cards left in deck", game.deck.cards_left()
-
-    test = game.deck
-    a = test.deal()
-    b = test.deal()
-    c = test.deal()
-    print game.deck.peek()
-    print test.peek()
-    print d.peek()
-    print game.deck == test.peek
-    print "Cards left in deck", game.deck.cards_left()
-    print "Cards left in test deck", test.cards_left()
--- cards/game.py
+++ cards/game.py
@@ -1,8 +1,9 @@
 from player import Player
+from deck import Deck
 
 class Game(object):
 
-    def __init__(self, deck, game_type=None):
+    def __init__(self, deck):
         self._players = {}
         self.deck = deck
 
@@ -17,3 +18,31 @@ class Game(object):
 
     def start_game(self):
         raise NotImplementedError
+
+class War(Game):
+    def __init__(self):
+        Game.__init__(self, Deck())
+        self._max_players = 2
+
+    def start_game(self):
+        print "Welcome to WAR!"
+        self.create_players()
+        print "The first player is:", self._players[0]['player'].name
+        print "The first player is:", self._players[1]['player'].name
+        print "Here is the scoreboard:"
+        self.print_scores()
+
+    def create_players(self):
+        name = raw_input("What is Player 1's name? ")
+        self.add_player(Player(name, 0))
+        name = raw_input("What is Player 2's name? ")
+        self.add_player(Player(name, 1))
+
+    def add_player(self, player):
+        if self.number_of_players() < self._max_players:
+            player_object = {'player': player, 'score': 0}
+            self._players[player.id()] = player_object
+
+    def print_scores(self):
+        for p_id, p_obj in self._players.iteritems():
+            print p_obj['player'].name + ': ' + str(p_obj['score'])
