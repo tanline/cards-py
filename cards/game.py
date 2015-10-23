e38b096b6b32676fe8e2bfa29db42cedaf2673e6
Tanpreet Grewal <tanpreet.grewal@gmail.com>
2015-10-23 08:45:32 -0400

    Remove GameBase and replace it with Game

--- cards/game.py
+++ cards/game.py
@@ -1,3 +1,17 @@
-from game_base import GameBase
-class Game(GameBase):
-    pass
+class Game(object):
+
+    def __init__(self, deck, game_type=None):
+        self._players = {}
+        self.deck = deck
+
+    def add_player(self, player):
+        self._players[player.get_id] = player
+
+    def remove_player(self, player):
+        del self._players[player.id]
+
+    def number_of_players(self):
+        return len(self._players)
+
+    def start_game(self):
+        raise NotImplementedError
